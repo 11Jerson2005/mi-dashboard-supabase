e0b5a487245679d80bf7d3f6f4d09e4b21185d57
Cristian Gabriel Zambrano Vega <cristian_uteq@hotmail.com>
2026-08-06 08:49:41 -0500

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <h1>Aplicación 1</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
